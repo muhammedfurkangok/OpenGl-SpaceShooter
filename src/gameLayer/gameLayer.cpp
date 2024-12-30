60c6a2912d72f6851824e60fc2281fd569275f8f
Muhammed Furkan Gok <gkkangk@gmail.com>
2024-12-30 18:24:12 +0300

    Project Init && Spaceship Included

---
+++ src/gameLayer/gameLayer.cpp
@@ -0,0 +1,68 @@
+#define GLM_ENABLE_EXPERIMENTAL
+#include "gameLayer.h"
+#include <glad/glad.h>
+#include <glm/glm.hpp>
+#include <glm/gtx/transform.hpp>
+#include "platformInput.h"
+#include "imgui.h"
+#include <iostream>
+#include <sstream>
+#include "imfilebrowser.h"
+#include <gl2d/gl2d.h>
+#include <platformTools.h>
+
+
+
+gl2d::Renderer2D renderer;
+gl2d::Texture spaceTexture;
+
+bool initGame()
+{
+	//initializing stuff for the renderer
+	gl2d::init();
+	renderer.create();
+
+	spaceTexture.loadFromFile(RESOURCES_PATH "spaceShip/ships/green.png", true);
+	
+	
+	return true;
+}
+
+
+
+bool gameLogic(float deltaTime)
+{
+#pragma region init stuff
+	int w = 0; int h = 0;
+	w = platform::getFrameBufferSizeX(); //window w
+	h = platform::getFrameBufferSizeY(); //window h
+	
+	glViewport(0, 0, w, h);
+	glClear(GL_COLOR_BUFFER_BIT); //clear screen
+
+	renderer.updateWindowMetrics(w, h);
+#pragma endregion
+
+
+
+	renderer.renderRectangle({100,100, 100, 100}, spaceTexture);
+
+
+	renderer.flush();
+
+
+	//ImGui::ShowDemoWindow();
+
+
+	return true;
+#pragma endregion
+
+}
+
+//This function might not be be called if the program is forced closed
+void closeGame()
+{
+
+
+
+}
